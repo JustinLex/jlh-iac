d17414657cdff2036e0009c403a921a71e48380c
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2020-11-05 12:48:31 +0100

    Add experimental nextcloud container

--- main.tf
+++ main.tf
@@ -7,3 +7,30 @@ terraform {
   }
   required_version = ">= 0.13"
 }
+
+provider "proxmox" {
+  pm_api_url      = "https://pve.jlh.name:8006/api2/json"
+  pm_user         = "root@pam"
+  pm_tls_insecure = true
+}
+
+resource "proxmox_lxc" "swablu" {
+  hostname = "swablu"
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "dhcp"
+    ip6    = "dhcp"
+  }
+  ostemplate   = "local:vztmpl/centos-8-default_20191016_amd64.tar.xz"
+  password     = "rootroot"
+  storage      = "local-zfs"
+  unprivileged = true
+  target_node  = "pve"
+  vmid         = 200
+  description  = "Testing terraform"
+
+
+  #preprovision = true
+  #os_type= "centos"
+}
