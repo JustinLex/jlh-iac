551160cc0417b8ede4a3551b328774ae1e5bf043
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2021-09-27 00:34:17 +0200

    Implement duplicacy install and initialization

--- roles/backup/files/backup.sh
+++ roles/backup/files/backup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Load B2 key and encryption password
+source /mnt/backup/passwords_rc
+
 # https://github.com/gilbertchen/duplicacy/wiki/backup
 # backup to local
+duplicacy backup -background -log -threads 4 -storage local > "/mnt/backup/logs/backup-$(date +"%Y-%m-%dZ%T")" 2>&1
 
 # copy to remote
 # https://github.com/gilbertchen/duplicacy/wiki/copy
-
+duplicacy copy -background -log -threads 10 -from local -to remote > "/mnt/backup/logs/copy-$(date +"%Y-%m-%dZ%T")" 2>&1
 
 # Notes:
 
--- roles/backup/files/initialize.sh
+++ roles/backup/files/initialize.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+# Load B2 key and encryption password
+source /mnt/backup/passwords_rc
 
 # init local
 # https://github.com/gilbertchen/duplicacy/wiki/init
-duplicacy init -background -encrypt -storage-name local solid /mnt/backup/duplicacy > "/mnt/backup/logs/init-$(date +"%Y-%m-%dZ%T")"
+duplicacy init -background -log -encrypt -storage-name local solid /mnt/backup/duplicacy > "/mnt/backup/logs/init-$(date +"%Y-%m-%dZ%T")" 2>&1
 
 # add remote
 # https://github.com/gilbertchen/duplicacy/wiki/add
-duplicacy add -background -encrypt -storage-name remote solid {URL}
+duplicacy add -background -log -encrypt -storage-name remote solid b2://jlh-ditto > "/mnt/backup/logs/add-$(date +"%Y-%m-%dZ%T")" 2>&1
 
 
 # Notes:
-# encryption password is stored in local config
+# encryption password is stored in passwords_rc
 # https://github.com/gilbertchen/duplicacy/wiki/Managing-Passwords
 
 # Use -background flag to suppress prompts
