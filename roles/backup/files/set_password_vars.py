58392190bed0a274dbd323b51d6f8192264564b8
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2021-11-23 00:39:40 +0100

    Fix password vars

--- roles/backup/files/set_password_vars.py
+++ roles/backup/files/set_password_vars.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 from pathlib import Path
 
 import typing
@@ -12,18 +12,20 @@ BACKUP_FILE_PATH = Path('/mnt/backup/passwords_rc.backup')
 
 def read_and_store_variable(envfile: typing.TextIO, variable_name: str) -> None:
     value = input(f"Enter the value for {variable_name}")
-    envfile.write(f'{variable_name}="{value}"\n')
+    envfile.write(f'export {variable_name}="{value}"\n')
 
 
 def main():
     # Move current variables to .backup"""
-    FILE_PATH.replace(BACKUP_FILE_PATH)
+    if FILE_PATH.exists():
+        FILE_PATH.replace(BACKUP_FILE_PATH)
 
     # list of variables that we need to track
     variables = [
-        'DUPLICACY_PASSWORD',
-        'DUPLICACY_B2_ID',
-        'DUPLICACY_B2_KEY'
+        'DUPLICACY_LOCAL_PASSWORD',
+        'DUPLICACY_REMOTE_PASSWORD',
+        'DUPLICACY_REMOTE_B2_ID',
+        'DUPLICACY_REMOTE_B2_KEY'
     ]
 
     # Create new file
