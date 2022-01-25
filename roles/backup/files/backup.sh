dbb46dae1dec65b122e35988bc22fe8ecd89b470
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-01-25 21:30:44 +0100

    Tee backup output

--- roles/backup/files/backup.sh
+++ roles/backup/files/backup.sh
@@ -5,11 +5,11 @@ source /mnt/backup/passwords_rc
 
 # https://github.com/gilbertchen/duplicacy/wiki/backup
 # backup to local
-duplicacy -background -log backup -threads 4 -storage local > "/mnt/backup/log/backup-$(date +"%Y-%m-%dZ%T")" 2>&1
+duplicacy -background -log backup -threads 4 -storage local 2>&1 | tee "/mnt/backup/log/backup-$(date +"%Y-%m-%dZ%T")"
 
 # copy to remote
 # https://github.com/gilbertchen/duplicacy/wiki/copy
-duplicacy -background -log copy -threads 10 -from local -to remote > "/mnt/backup/log/copy-$(date +"%Y-%m-%dZ%T")" 2>&1
+duplicacy -background -log copy -threads 10 -from local -to remote 2>&1 | tee "/mnt/backup/log/copy-$(date +"%Y-%m-%dZ%T")"
 
 # Notes:
 
